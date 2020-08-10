ea6b1c35d87040948c499de8db3daef800915867
shine_xyy <1518030666@qq.com>
2020-08-11 00:42:15 +0800

    添加404页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
     Labels.vue
+    <Nav/>
 </div>
 </template>
 <script lang='ts'>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -5,10 +5,10 @@
 </div>
 </template>
 <script lang='ts'>
-import Nav from "@/components/Nav.vue";
+
     export default{
         name:'Money',
-        components: {Nav},
+
     };
 </script>
 <style lang="scss" scoped>
