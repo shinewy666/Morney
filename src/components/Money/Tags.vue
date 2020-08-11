ba7333e8b534ad7cf675267550fd08518576cb5c
shine_xyy <1518030666@qq.com>
2020-08-11 19:08:07 +0800

    模块化：把Money.vue分成四个子组件

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="tags"> 
+        <ul class="current">
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+        </ul>
+        <div class="new">
+            <button>新增标签</button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name:'Tags'
+    }
+</script>
+
+<style lang="scss" scoped>
+    .tags{
+        font-size: 14px;
+        padding: 16px;
+        > .current {
+            display: flex;
+            > li{
+                background: #d9d9d9;
+                height: 24px;
+                line-height: 24px;
+                border-radius: (24px/2);
+                padding: 0 16px;
+                margin-right: 12px;
+            }
+        }
+        > .new{
+            padding-top:16px;
+            button{
+                background: transparent;
+                border: none;
+                color: #999;
+                border-bottom: 1px solid;
+            }
+        }
+    }
+</style>
