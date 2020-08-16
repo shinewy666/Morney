6563ec81dde09cfb33f271c35f4c3606124645dd
shine_xyy <1518030666@qq.com>
2020-08-16 23:16:46 +0800

    优化

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -11,23 +11,40 @@
     </div>
 </template>
 
-<script>
-    export default {
-        name:'Types',
-        data(){
-            return{
-                type: '-'   /*'-'表示支出 '+'表示收入*/
-            }
-        },
-        methods:{
-            selectType(type){  /* type只能是'-'和'+'中的一个*/
-                if(type !=='-' && type !=='+'){
-                    throw new Error('type is unknown')
-                }
-                this.type=type
-            }
-        }
+<script lang="ts">
+import Vue from 'vue'
+import {Component} from 'vue-property-decorator';
+
+@Component({
+    props:{
+        propMessage:String
     }
+})
+export default class Types extends Vue{
+    type = '-'; /*'-'表示支出 '+'表示收入*/
+    selectType(type: string){  /* type只能是'-'和'+'中的一个*/
+                 if(type !=='-' && type !=='+'){
+                     throw new Error('type is unknown')
+                 }
+                 this.type=type
+             }
+}
+    // export default {
+    //     name:'Types',
+    //     data(){
+    //         return{
+    //             type: '-'   /*'-'表示支出 '+'表示收入*/
+    //         }
+    //     },
+    //     methods:{
+    //         selectType(type){  /* type只能是'-'和'+'中的一个*/
+    //             if(type !=='-' && type !=='+'){
+    //                 throw new Error('type is unknown')
+    //             }
+    //             this.type=type
+    //         }
+    //     }
+    // }
 </script>
 
 <style lang="scss" scoped>
