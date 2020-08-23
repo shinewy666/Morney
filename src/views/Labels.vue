643fa0c5fff9c99d14981df890d8136315de69a3
shine_xyy <1518030666@qq.com>
2020-08-23 22:33:29 +0800

    新增标签跳转页

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,48 +1,43 @@
-import Icon from '@/components/Icon.vue';
-import Labels from '@/views/Labels.vue';
 <template>
   <Layout>
-    <ol class="tags">
-      <li v-for="tag in tags" :key="tag.id">
+    <div class="tags">
+      <router-link class="tag" v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${tag.id}`">
         <span>{{tag.name}}</span>
         <Icon name="right" />
-      </li>
-    </ol>
+      </router-link>
+    </div>
     <div class="creatTag-wrapper">
       <button class="creatTag" @click="createTag">新建标签</button>
     </div>
   </Layout>
 </template>
 <script lang='ts'>
-import Vue from 'vue'
-import { Component } from 'vue-property-decorator';
-import tagListModel from '@/components/models/tagListModel';
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import tagListModel from "@/components/models/tagListModel";
 
-
-
-tagListModel.fetch()
+tagListModel.fetch();
 @Component
-export default class Labels extends Vue{
-  tags = tagListModel.data
-  createTag(){
-      const name = window.prompt('请输入标签名')
-      if(name){
-          const message = tagListModel.create(name)
-          if(message === 'duplicated'){
-              window.alert('标签名重复')
-          }else if(message === 'success'){
-              window.alert('新增标签成功')
-          }
+export default class Labels extends Vue {
+  tags = tagListModel.data;
+  createTag() {
+    const name = window.prompt("请输入标签名");
+    if (name) {
+      const message = tagListModel.create(name);
+      if (message === "duplicated") {
+        window.alert("标签名重复");
+      } else if (message === "success") {
+        window.alert("新增标签成功");
       }
+    }
   }
 }
-
 </script>
 <style lang="scss" scoped>
 .tags {
   background: white;
   padding-left: 16px;
-  > li {
+  > .tag {
     min-height: 44px;
     display: flex;
     align-items: center;
