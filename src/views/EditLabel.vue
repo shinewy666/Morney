6038822dc3de9d9df8d9702a98abbaa53624acca
shine_xyy <1518030666@qq.com>
2020-08-23 22:23:58 +0800

    新增 EditLabel 组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,15 +1,28 @@
+import EditLabel from '@/views/EditLabel.vue';
 <template>
-    <div>
-        编辑标签
-    </div>
+  <div>编辑标签</div>
 </template>
 
-<script>
-    export default {
-        
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import tagListModel from "../components/models/tagListModel";
+
+@Component
+export default class EditLabel extends Vue {
+  created() {
+    const id = this.$route.params.id;
+    tagListModel.fetch();
+    const tags = tagListModel.data;
+    const tag = tags.filter((t) => t.id === id)[0];
+    if (tag) {
+      console.log(tag);
+    } else {
+      this.$router.replace("/404");
     }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
