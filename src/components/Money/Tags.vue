839f7fdecb5b15ac2a48a3629dc32a4997b310c1
shine_xyy <1518030666@qq.com>
2020-08-30 21:23:37 +0800

    修复 Tags.vue 的 bug

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,77 +1,75 @@
 <template>
-    <div class="tags"> 
-        <ul class="current">
-            <li v-for="tag in dataSource" :key="tag.id"
-            :class="{selected:selectedTags.indexOf(tag)>=0}" 
-            @click="toggle(tag)">{{tag.name}}</li>
-        </ul>
-        <div class="new">
-            <button @click="create">新增标签</button>
-        </div>
+  <div class="tags">
+    <ul class="current">
+      <li
+        v-for="tag in tagList"
+        :key="tag.id"
+        :class="{selected:selectedTags.indexOf(tag)>=0}"
+        @click="toggle(tag)"
+      >{{tag.name}}</li>
+    </ul>
+    <div class="new">
+      <button @click="create">新增标签</button>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import {Component,Prop} from 'vue-property-decorator'
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import tagStore from "../../store/tagStore";
 @Component
-    export default class Tags extends Vue{
-        @Prop() readonly dataSource: string[] | undefined;
-        //Prop告诉Vue,dataSource 不是 data 是 prop
-        //String 告诉 Vue xxx 运行时是个 String 
-        //dataSource 属性名
-        //string | undefined 告诉 TS xxx 的编译时类型
-        selectedTags: string[] = [];
+export default class Tags extends Vue {
+  tagList = tagStore.fetchTags();
+  selectedTags: string[] = [];
 
-        toggle(tag: string){
-            const index = this.selectedTags.indexOf(tag)
-            if(index>=0){
-                this.selectedTags.splice(index,1)
-            }else{
-                this.selectedTags.push(tag);
-            }
-            this.$emit('update:value',this.selectedTags)
-        }
-        create(){
-            const name = window.prompt('请输入标签名');
-            if(name==''){
-                window.alert('标签名不能为空')
-            }else if(this.dataSource){
-                    this.$emit('update:dataSource',[...this.dataSource,name])
-                }
-            
-        }
+  toggle(tag: string) {
+    const index = this.selectedTags.indexOf(tag);
+    if (index >= 0) {
+      this.selectedTags.splice(index, 1);
+    } else {
+      this.selectedTags.push(tag);
     }
+    this.$emit("update:value", this.selectedTags);
+  }
+  create() {
+    const name = window.prompt("请输入标签名");
+    if (!name) {
+      return window.alert("标签名不能为空");
+    }
+    tagStore.createTag(name);
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-    .tags{
-        font-size: 14px;
-        padding: 16px;
-        > .current {
-            display: flex;
-            > li{
-                $bg:#d9d9d9;
-                background: $bg;
-                height: 24px;
-                line-height: 24px;
-                border-radius: (24px/2);
-                padding: 0 16px;
-                margin-right: 12px;
-                &.selected{
-                    background: darken($bg,50%);
-                    color: white;
-                }
-            }
-        }
-        > .new{
-            padding-top:16px;
-            button{
-                background: transparent;
-                border: none;
-                color: #999;
-                border-bottom: 2px solid;
-            }
-        }
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  > .current {
+    display: flex;
+    > li {
+      $bg: #d9d9d9;
+      background: $bg;
+      height: 24px;
+      line-height: 24px;
+      border-radius: (24px/2);
+      padding: 0 16px;
+      margin-right: 12px;
+      &.selected {
+        background: darken($bg, 50%);
+        color: white;
+      }
     }
+  }
+  > .new {
+    padding-top: 16px;
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 2px solid;
+    }
+  }
+}
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -5,7 +5,7 @@
             <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
             <Types :value.sync="record.type"/>
             <Notes field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes"/>
-            <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
+            <Tags/>
         </Layout>
 
 </template>
@@ -17,20 +17,6 @@
     import Tags from '@/components/Money/Tags.vue';
     import { Component} from 'vue-property-decorator';
     import store from '../store/index2';
-
-    // const version = store.localStorage.getItem('version') || '0'
-    
-    
-
-    //     if(version === '0.0.1'){
-    //         //数据库升级，数据迁移
-    //         store.recordList.forEach(record=>{
-    //             record.createdAt = new Date(2020,0,1)
-    //         })
-    //         window.localStorage.setItem('recordList',JSON.stringify(store.recordList))
-    //     }
-    
-    // window.localStorage.setItem('version','0.0.2')//数据库版本
 
     type RecordItem = {
         tags: string[];
@@ -44,25 +30,16 @@
     })
     export default class Money extends Vue{
 
-        tags=store.tagList;
+
         recordList=store.recordList
         record: RecordItem={tags:[], notes:'',type:'-',amount:0}
 
-        onUpdateTags(value: string[]){
-            this.record.tags=value
-        }
+
         onUpdateNotes(value: string){
             this.record.notes=value
             
         }
-        // onUpdateType(value: string){
-        //     this.record.type=value
-            
-        // }
-        // onUpdateAmount(value: string){
-        //     this.record.amount=parseFloat(value)
-            
-        // }
+
         saveRecord(){
             store.createRecord(this.record)
         }
