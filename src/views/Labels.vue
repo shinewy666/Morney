ccf358d5bce18c7295500412971b7657622c53ca
shine_xyy <1518030666@qq.com>
2020-09-08 01:00:03 +0800

    重构 Tags.vue 和 Labels.vue

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -18,15 +18,23 @@ import Button from "@/components/Button.vue";
 
 
 @Component({
-  components:{Button}
+  components:{Button},
+  computed:{
+    tags(){
+      return this.$store.state.tagList
+    }
+  }
 })
 export default class Labels extends Vue {
-  tags = []; //store.tagList;
+  beforeCreate(){
+    this.$store.commit('fetchTags')
+  }
   createTag() {
     const name = window.prompt("请输入标签名");
-    if(name){
-      // store.createTag(name)
+    if (!name) {
+      return window.alert("标签名不能为空");
     }
+    this.$store.commit('createTag',name)
   }
 }
 </script>
